46e9b89802c6ae06f98bf75cfcc9215f4ee96bc6
ibtisamath <ibtisamathsoumanou1@gmail.com>
2024-06-02 07:10:44 +0200

    img

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./public/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
